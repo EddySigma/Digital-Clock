c4c423cace159fa9a79e99501bba71d622b67ac2
Ed <deved.gm@gmail.com>
2023-04-08 18:27:29 -0700

    Got the clock to update. Next thing to do is organize things better.

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -2,89 +2,67 @@
 # ^ this should work fine in a virtual environment
 
 import tkinter as tk
+import datetime
+import time
+    
+class DigitalClockFrame():
+    def __init__(self, parent) -> None:
+        self.parent = parent
 
-class Digi_Clock(tk.Tk):
-    # initial settings
-    border_width = 20
-    overall_font = 0
-    clock_font = "Arial" # placeholder
-    time_font_size = 35
-    am_pm_font_size = 20
-    current_time = "12:34"
-    isAm = True
-    today = "April 3, 2023" # placeholder
-
-    def __init__(self):
-        super().__init__()
-
-        # root window configuration
-        self.title("Digital Clock")
-        self.geometry('250x150')
-
+        self.parent.title("Digital Clock App")
+        self.parent.geometry('250x150') #x,y
+        self.parent.resizable(False, False) #x,y
+        
         # Digital clock face is contained here
-        self.clock_face = tk.Frame(master=self, width=200, height=150)
+        self.clock_face = tk.Frame(master=self.parent)
         self.clock_face.pack(fill=tk.BOTH, expand=True)
         self.clock_face.pack_propagate(False)
-
+        
         # top row
-        self.top_row = tk.Frame(master=self.clock_face, height=self.border_width, bg="red")
+        self.top_row = tk.Frame(master=self.clock_face, height=20, bg="red")
         self.top_row.pack(fill=tk.X, side=tk.TOP)
-
-        # date/other
-        self.title_label = tk.Label(master=self.top_row, text=self.today)
+        
+        # Today's date
+        self.title_label = tk.Label(
+            master=self.top_row,
+            text=datetime.datetime.today().strftime("%B %d, %Y"),
+            bg="red")
         self.title_label.pack(side=tk.TOP)
-
+        
         # middle row
         self.mid_row = tk.Frame(master=self.clock_face)
         self.mid_row.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
         self.mid_row.pack_propagate(False)
 
         # will be used to house a cycling button
-        self.left_column = tk.Frame(master=self.mid_row, width=self.border_width, bg='blue')
+        self.left_column = tk.Frame(master=self.mid_row, width=20)
         self.left_column.pack(fill=tk.Y, side=tk.LEFT)
         self.left_column.pack_propagate(False)
 
         self.left_btn = tk.Button(master=self.left_column, text="<")
-        self.left_btn.pack(side=tk.LEFT)
+        self.left_btn.pack(side=tk.LEFT, fill=tk.BOTH)
 
-        # holds the actual clock display (numbers + am/pm)
-        self.clock_frame = tk.Frame(master=self.mid_row, bg="yellow")
-        self.clock_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        self.clock_frame.pack_propagate(False)
-
-        # clock time
-        self.clock_time = tk.Label(master=self.clock_frame, text=self.current_time, font=(self.clock_font, self.time_font_size))
-        self.clock_time.pack(side=tk.LEFT)
-
-        # time state
-        self.time_state = tk.Frame(master=self.clock_frame)
-        self.time_state.pack(side=tk.RIGHT)
-
-        # am
-        am_label = tk.Label(master=self.time_state, text="am", font=(self.clock_font, self.am_pm_font_size))
-        am_label.pack(side=tk.TOP)
-
-        # pm
-        self.pm_label = tk.Label(master=self.time_state, text="pm", font=(self.clock_font, self.am_pm_font_size))
-        self.pm_label.pack(side=tk.BOTTOM)
+        # TODO: insert the utility frame here ==========================
+        Clock(self.mid_row)
 
         # will be used to house a cycling button
-        self.right_column = tk.Frame(master=self.mid_row, width=self.border_width, bg='blue')
+        self.right_column = tk.Frame(master=self.mid_row, width=20)
         self.right_column.pack(fill=tk.Y, side=tk.RIGHT)
         self.right_column.pack_propagate(False)
-
+        
         self.right_btn = tk.Button(master=self.right_column, text=">")
-        self.right_btn.pack(side=tk.RIGHT)
+        self.right_btn.pack(side=tk.RIGHT, fill=tk.BOTH)
 
         # bottom row
-        self.bottom_row = tk.Frame(master=self.clock_face, height=self.border_width)
+        self.bottom_row = tk.Frame(master=self.clock_face, height=20)
         self.bottom_row.pack(fill=tk.X, side=tk.BOTTOM)
         self.bottom_row.pack_propagate(False)
 
+        # TODO: replace with function that takes a list of items
         # button section
         self.button_area = tk.Frame(master=self.bottom_row)
         self.button_area.pack(side=tk.BOTTOM)
-
+        """
         self.clock_btn = tk.Button(master=self.button_area, text="Clock")
         self.clock_btn.pack(side=tk.LEFT)
 
@@ -93,10 +71,48 @@ class Digi_Clock(tk.Tk):
 
         self.stop_watch_btn = tk.Button(master=self.button_area, text="Stop Watch")
         self.stop_watch_btn.pack(side=tk.LEFT)
+        """
+    def bottom_btn_row(): # takes list of buttons
+        return 1
+    
+class Clock(tk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.overall_font = 0
+        self.clock_font = "Arial" # placeholder
+        self.time_font_size = 35
+        self.am_pm_font_size = 20
+        self.isAm = True
+
+        # holds the actual clock display (numbers + am/pm)
+        self.clock_frame = tk.Frame(master=container)
+        self.clock_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+        self.clock_frame.pack_propagate(False)
+
+        # clock time
+        # """24hr format""" self.current_time = datetime.now().strftime("%H:%M")
+        #self.current_time = datetime.datetime.now().time().strftime("%I:%M %p") # %p is for am/pm
+
+        def time():
+            current_time = datetime.datetime.now().time().strftime("%H:%M:%S %p")
+            self.clock_time.config(text=current_time)
+            self.clock_time.after(1000, time)
+        
+        self.clock_time = tk.Label(
+            master=self.clock_frame,
+            font=(self.clock_font, self.time_font_size))
+        self.clock_time.pack(side=tk.LEFT)
+
+        time()
+
+    def time_now(): # returns a tuple time and isAm -> T/F
+        return 
 
 
 if __name__ == "__main__":
-    app = Digi_Clock()
+    app = tk.Tk()
+    DigitalClockFrame(app)
     app.mainloop()
 
 
